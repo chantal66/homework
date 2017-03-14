a81f2eefd62ce2701c4e9e900c2df6ccb6df8b40
Chantal Justamond <cjustamm66@me.com>
2017-03-14 15:27:14 -0600

    rando_guesser working

---
+++ rando-guesser.rb
@@ -0,0 +1,16 @@
+# Create a guessing game.
+# The computer picks a random number. From 0-10
+# Player enters a guess.
+# If they get it correct, the computer will let them know and
+# end the game.
+
+guess = ""
+secret_number = rand(0..10) 
+
+while guess != secret_number
+  puts 'guess my number:'
+  guess = gets.chomp.to_i
+
+  puts "guess is #{guess}"
+
+end
