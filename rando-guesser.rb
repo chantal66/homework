32e85721b0e8d96beacd0b2373a4e126d82152c2
Chantal Justamond <cjustamm66@me.com>
2017-03-14 15:32:47 -0600

    second implementation of rando-guesser

--- rando-guesser.rb
+++ rando-guesser.rb
@@ -4,13 +4,24 @@
 # If they get it correct, the computer will let them know and
 # end the game.
 
-guess = ""
-secret_number = rand(0..10) 
+# guess = ""
+# secret_number = rand(0..10) 
 
-while guess != secret_number
-  puts 'guess my number:'
+# while guess != secret_number
+#   puts 'guess my number:'
+#   guess = gets.chomp.to_i
+
+#   puts "your guess is #{guess}"
+
+# end
+
+guess = ''
+secret_number = rand(0..10)
+until guess == secret_number
+  puts 'guess my number: '
   guess = gets.chomp.to_i
 
-  puts "guess is #{guess}"
+  puts "your guess is: #{guess}"
 
 end
+puts 'GoodBye!!'
