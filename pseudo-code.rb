bb779ad705678a43d95c5a9843e96ed3e8f32169
Chantal Justamond <cjustamm66@me.com>
2017-03-14 12:59:28 -0600

    seudocode

---
+++ pseudo-code.rb
@@ -0,0 +1,11 @@
+# have to be able to wait for input (in this case enter) something like speed 0.5s
+# set up auto response to HELLO?
+# get info from user
+# set up a (if) for lower case question
+# auto response with I AM HAVING A HARD TIME HEARING YOU
+# set up (if) case upcase letter question 
+# set up auto response NO, THIS IS NOT A PET STORE
+# WHEN first "goodbye" (something like a counter)
+# set up response for ANYTHING ELSE I CAN HELP WITH?
+# WHEN seconde "goodbye" respond THANK YOU FOR CALLING 
+# and exit after thank you for calling.
